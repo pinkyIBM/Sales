712f4c5097b67ba04c736e60e291d03f695382a5
Pinky <Pinky@pimittal.in.ibm.com>
2017-02-13 15:17:33 +0530

    Initial Checkin of sales

---
+++ SalesDemo/src/com/Main.java
@@ -0,0 +1,20 @@
+package com;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        SalesData data = new SalesData();
+        display();
+        data.SalesDisplay();
+	}
+
+	
+
+	private static void display() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Sales Team");
+		
+	}
+
+}
---
+++ SalesDemo/src/com/SalesData.java
@@ -0,0 +1,11 @@
+package com;
+
+public class SalesData {
+
+	public void SalesDisplay() {
+		// TODO Auto-generated method stub
+		System.out.println("Sales Data needs to be entered");
+		
+	}
+
+}
